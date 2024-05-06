6affeb42c99a3e1e6f55de728643b32e07048222
andrewbloese-00 <andrewbloese@gmail.com>
2024-05-06 13:52:24 -0700

    Removed debug statements from limit.js

--- limit.js
+++ limit.js
@@ -4,7 +4,6 @@ export const IPSelector = req => req.socket.remoteAddress
 export function getRateLimiter(nRequests,selector=IPSelector,cooldown_ms=60_000){
 	const r = {}
 	return (req,res,next) => {
-		console.table(r)
 		const key = selector(req)
 		const now = Date.now()
 		if(!r[key]) r[key] = [now]
